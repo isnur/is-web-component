a7fbdaa35bc8825a5b225b98dbcde52713f5dc3e
Isnur Muhammad Suryo Margono <isnurmsm@gmail.com>
2019-10-07 12:50:58 +0700

    upload docs folder

--- www/build/is-web-component.esm.js
+++ www/build/is-web-component.esm.js
@@ -1,6 +1,1 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-e26a522c.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["is-badge",[[2,"is-badge",{"color":[1],"text":[1],"rounded":[4]}]]],["is-multiselect",[[2,"is-multiselect",{"disabled":[4],"placeholder":[1],"items":[16],"selected":[16],"labelTo":[16],"limit":[2],"limitText":[16],"labelSelected":[1,"label-selected"],"showSelectedBadge":[4,"show-selected-badge"],"isExpanded":[32],"keyword":[32],"textSelected":[32],"tags":[32],"getSelected":[64]},[[4,"click","handleClick"]]]]]], options);
-});
+import{p as e,b as l}from"./p-76388117.js";e().then(e=>l([["p-iyotoov5",[[2,"is-multiselect",{disabled:[4],placeholder:[1],items:[16],selected:[16],labelTo:[16],limit:[2],limitText:[16],labelSelected:[1,"label-selected"],showSelectedBadge:[4,"show-selected-badge"],isExpanded:[32],keyword:[32],textSelected:[32],tags:[32],getSelected:[64]},[[4,"click","handleClick"]]],[2,"is-badge",{color:[1],text:[1],rounded:[4]}]]]],e));
